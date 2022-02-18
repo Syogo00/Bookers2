fee294216faa02d3cc09f988f35534dd21c6bcb3
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-02-18 15:14:16 +0000

    {update}課題3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parmitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resourse)
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_oath_for(resourse)
+    root_path
+  end
+
+  protected
+
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(sign_up, keys: [:email])
+  end
+
 end
