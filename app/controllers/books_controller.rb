1d0d19f6b9cdc38e9028f94d71a1e10ed0dc3884
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-02-20 10:01:31 +0000

    {update}課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,27 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
+
   def create
-    @book = Book.new(book.params)
-    @book.user_id = currnt_user.id
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book)
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
     @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = current_user
   end
-  
+
   private
 
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
