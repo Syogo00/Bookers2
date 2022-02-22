87781a0edec118b4b7a2e2066216eb26e8735bc5
EC2 Default User <ec2-user@ip-172-31-86-83.ec2.internal>
2022-02-22 03:12:52 +0000

    {update}課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,13 +19,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path, notice: "You have updated user successfully."
+    if @user.update(user_params)
+      redirect_to user_path, notice: "You have updated user successfully."
+    else
+      render :edit
+    end  
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
